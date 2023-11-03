5c1463ce35fe6554e7a2046bc0c62fa27c9a8a96
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2023-11-03 20:43:28 -0300

    component drinkCard y cocktail-carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CocktailCarouselComponent } from './components/cocktail-carousel/cocktail-carousel.component';
+import { DrinkCardComponent } from './components/drink-card/drink-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CocktailCarouselComponent,
+    DrinkCardComponent
   ],
   imports: [
     BrowserModule,
