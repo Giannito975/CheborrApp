efb532b5cd859bfd785487182550580fa478a82d
Gianni <gianniricciardi@3779@gmail.com>
2023-11-14 23:11:28 -0300

    Creada logica de alertas para las interacciones con los tragos

--- src/app/components/cocteleria/coctel-list/coctel-list.component.ts
+++ src/app/components/cocteleria/coctel-list/coctel-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CocktailAPIService } from 'src/app/services/cocktail-api.service';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import Swal from 'sweetalert2';
 
 
@@ -15,41 +15,54 @@ export class CoctelListComponent implements OnInit {
 
   //public alcoholicCoctailList:any = [];
   //public nonAlcoholicCoctailList:any = [];
-  public allCocktailsList:any = [];
+  public allCocktailsList: any = [];
   filterDrink = '';
 
-  constructor(private CocktailAPIService:CocktailAPIService){}
+  constructor(private CocktailAPIService: CocktailAPIService) { }
 
   ngOnInit(): void {
     this.loadAlcoholicDrinks();
     this.loadNonAlcoholicDrinks();
   }
 
-  loadAlcoholicDrinks(){
+  loadAlcoholicDrinks() {
     this.CocktailAPIService.getAllAlcoholicDrinks()
-    .subscribe(respuesta => {
-      this.allCocktailsList = respuesta;
-    })
+      .subscribe(respuesta => {
+        this.allCocktailsList = respuesta;
+      })
   }
 
-  loadNonAlcoholicDrinks(){
+  loadNonAlcoholicDrinks() {
     this.CocktailAPIService.getAllNonAlcoholicDrinks()
-    .subscribe(respuesta => {
-      this.allCocktailsList = respuesta;
-    })
+      .subscribe(respuesta => {
+        this.allCocktailsList = respuesta;
+      })
   }
 
-  addCocktailToBodegaPersonal(){
-    let flag = 0;
-    //si sale todo bien, tiramos el sweet alert positivo
-    if(flag == 0){
-      this.positiveAddCocktailAlert();
-    }else{ //si no, el negativo
-      this.negativeAddCocktailAlert();
+  addCocktailToBodegaPersonal() {
+    let loggedUser = true;
+    
+      //si esta logueado, puede agregar tragos a su bodega personal
+    if (loggedUser) {
+      //si el trago no está en su bodega personal, lo agregaría y damos la alerta que se agregó
+      if (loggedUser) {
+        this.positiveAddCocktailAlert();
+      } else { ////si el trago ESTÁ en su bodega personal, le decimos q no puede agregarlo porque ya lo tiene
+        this.negativeAddCocktailAlert();
+      }
+    }else{ //si NO está logueado, le decimos q para agregar tragos a su bodega personal se tiene que loguear
+      Swal.fire({
+        title: "<strong><u>¡ATENCION!</u></strong>",
+        icon: "info",
+        html: `
+          Para agregar un trago a tu Bodega Personal necesitas estar logueado
+        `,
+        focusConfirm: false,
+      });
     }
   }
 
-  positiveAddCocktailAlert() : void{
+  positiveAddCocktailAlert(): void {
     Swal.fire({
       icon: "success",
       title: "Trago agregado a tu Bodega Personal",
@@ -58,11 +71,11 @@ export class CoctelListComponent implements OnInit {
     });
   }
 
-  negativeAddCocktailAlert() : void{
+  negativeAddCocktailAlert(): void {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: "No pudo agregarse el trago"
+      text: "El trago ya pertenece a tu Bodega Personal"
     });
   }
 
