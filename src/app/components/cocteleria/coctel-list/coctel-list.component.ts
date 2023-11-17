3971af94b7ccdd701135913f41daf743344ee643
Gianni <gianniricciardi@3779@gmail.com>
2023-11-16 23:11:48 -0300

    Finalizada la corrección del bug que no nos permitía agregar un trago a la bodega personal del usuario logueado

--- src/app/components/cocteleria/coctel-list/coctel-list.component.ts
+++ src/app/components/cocteleria/coctel-list/coctel-list.component.ts
@@ -8,122 +8,91 @@ import { BodegaPersonalService } from 'src/app/services/bodega-personal-api.serv
 import { BodegaPersonal, User } from 'src/app/services/models';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersApiService } from 'src/app/services/users-api.service';
-
+import { IBodegaPersonal } from 'src/app/services/interface';
+import { of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-coctel-list',
   templateUrl: './coctel-list.component.html',
-  styleUrls: ['./coctel-list.component.css']
+  styleUrls: ['./coctel-list.component.css'],
 })
 export class CoctelListComponent implements OnInit {
-
-  //public alcoholicCoctailList:any = [];
-  //public nonAlcoholicCoctailList:any = [];
-  bodegaPersonalService : BodegaPersonalService | undefined;
-
   public allCocktailsList: any = [];
   filterDrink = '';
 
-  constructor(private CocktailAPIService: CocktailAPIService, private authService: AuthService, public usersApiService: UsersApiService) { }
+  constructor(
+    private CocktailAPIService: CocktailAPIService,
+    private authService: AuthService,
+    public usersApiService: UsersApiService,
+    private readonly bodegaPersonalService: BodegaPersonalService
+  ) {}
 
   ngOnInit(): void {
-    this.loadAlcoholicDrinks();
-    this.loadNonAlcoholicDrinks();
+    this.loadDrinks();
   }
 
-  loadAlcoholicDrinks() {
+  loadDrinks(): void {
+    // Load multiple drinks using switchMap
     this.CocktailAPIService.getAllAlcoholicDrinks()
-      .subscribe(respuesta => {
-        this.allCocktailsList = respuesta;
-      })
+      .pipe(
+        switchMap((alcoholicDrinks) => {
+          console.log({ alcoholicDrinks });
+          return this.CocktailAPIService.getAllNonAlcoholicDrinks().pipe(
+            switchMap((nonAlcoholicDrinks) => {
+              console.log({ nonAlcoholicDrinks });
+              return of([...alcoholicDrinks, ...nonAlcoholicDrinks]);
+            })
+          );
+        })
+      )
+      .subscribe((drinks) => {
+        console.log({ drinks });
+        this.allCocktailsList = drinks;
+      });
   }
-
-  loadNonAlcoholicDrinks() {
-    this.CocktailAPIService.getAllNonAlcoholicDrinks()
-      .subscribe(respuesta => {
-        this.allCocktailsList = respuesta;
-      })
-  }
-
-  /*addCocktailToBodegaPersonal(idDrink : any) {  *********************          INTENTO 1 NO FUNCIONA
-    let loggedUser = this.authService.getUserFromLocalStorage();
-    const bodegaPersonalNueva = new BodegaPersonal();
-    bodegaPersonalNueva.userId = loggedUser?.id;
-    bodegaPersonalNueva.drinkId = idDrink;
-    if (loggedUser != null) {
-      //si el trago no está en su bodega personal, lo agregaría y damos la alerta que se agregó
-      if (loggedUser) {
-        
-        //aca tendria que agregar la validacion para verificar si el trago ya existe en la bodega personal del usuario
-        
-        this.bodegaPersonalService?.addCocktailToBodegaPersonal(bodegaPersonalNueva)
-        this.positiveAddCocktailAlert();
-      } else { ////si el trago ESTÁ en su bodega personal, le decimos q no puede agregarlo porque ya lo tiene
-        this.negativeAddCocktailAlert();
-      }
-    }else{ //si NO está logueado, le decimos q para agregar tragos a su bodega personal se tiene que loguear
-      this.pleaseLogInAlert;
-    }
-  }*/
-
-  /*addCocktailToBodegaPersonal(idDrink: any) { ********************** INTENTO 2 TAMPOCO FUNCIONA
-    const loggedUser = this.authService.getUserFromLocalStorage();
-  
-    if (loggedUser) {
-      const bodegaPersonalNueva = new BodegaPersonal();
-      bodegaPersonalNueva.userId = loggedUser.id;
-      bodegaPersonalNueva.drinkId = idDrink;
-  
-      // Verificar si el trago ya existe en la bodega personal del usuario
-      const tragoYaExiste = this.bodegaPersonalService?.tragoYaExisteEnBodegaPersonal(idDrink);
-  
-      if (!tragoYaExiste) {
-        // Si el trago no está en su bodega personal, lo agregaría y damos la alerta que se agregó
-        this.bodegaPersonalService?.addCocktailToBodegaPersonal(bodegaPersonalNueva);
-        this.positiveAddCocktailAlert();
-      } else {
-        // Si el trago YA está en su bodega personal, le decimos que no puede agregarlo porque ya lo tiene
-        this.negativeAddCocktailAlert();
-      }
-    } else {
-      // Si NO está logueado, le decimos que para agregar tragos a su bodega personal se tiene que loguear
-      this.pleaseLogInAlert();
-    }
-  }*/
 
   public async addCocktailToBodegaPersonal(idDrink: any): Promise<void> {
-   
-      const loggedUser = this.authService.getUserFromLocalStorage();
-  
-      if (!loggedUser) {
-        // Si NO está logueado, le decimos que para agregar tragos a su bodega personal se tiene que loguear
-        this.pleaseLogInAlert();
-        return;
-      }else{
-        const bodegaPersonalNueva = new BodegaPersonal();
-      bodegaPersonalNueva.userId = loggedUser.id;
-      bodegaPersonalNueva.drinkId = idDrink;
-        
-      // Verifica si el trago ya existe en la bodega personal del usuario
-      const tragoYaExiste = await this.bodegaPersonalService?.tragoYaExisteEnBodegaPersonal(bodegaPersonalNueva);
-  
-      if (!tragoYaExiste) {
-        // Si el trago no está en su bodega personal, lo agregaría y damos la alerta que se agregó
-        await this.bodegaPersonalService?.addCocktailToBodegaPersonal(bodegaPersonalNueva);
-        this.positiveAddCocktailAlert();
-      } else {
-        // Si el trago YA está en su bodega personal, le decimos que no puede agregarlo porque ya lo tiene
-        this.negativeAddCocktailAlert();
-      }
-      }
-  }
-  
-  
+    const loggedUser = this.authService.getUserFromLocalStorage();
 
-  pleaseLogInAlert() : void {
+    if (!loggedUser) {
+      // Si NO está logueado, le decimos que para agregar tragos a su bodega personal se tiene que loguear
+      this.pleaseLogInAlert();
+    } else {
+      const bodegaPersonalNueva: IBodegaPersonal = {
+        userId: loggedUser.id,
+        drinkId: idDrink,
+      };
+      console.log({ bodegaPersonalNueva });
+
+      // Verifica si el trago ya existe en la bodega personal del usuario
+      // const tragoYaExiste =
+      //   await this.bodegaPersonalService?.tragoYaExisteEnBodegaPersonal(
+      //     bodegaPersonalNueva
+      //   );
+      this.bodegaPersonalService
+        .tragoYaExisteEnBodegaPersonal(bodegaPersonalNueva)
+        .subscribe((tragoYaExiste) => {
+          console.log({ tragoYaExiste });
+          if (!tragoYaExiste) {
+            // Si el trago no está en su bodega personal, lo agregaría y damos la alerta que se agregó
+            this.bodegaPersonalService
+              .addCocktailToBodegaPersonal(bodegaPersonalNueva)
+              .subscribe((response) => {
+                console.log({ response });
+                this.positiveAddCocktailAlert();
+              });
+          } else {
+            // Si el trago YA está en su bodega personal, le decimos que no puede agregarlo porque ya lo tiene
+            this.negativeAddCocktailAlert();
+          }
+        });
+    }
+  }
+
+  pleaseLogInAlert(): void {
     Swal.fire({
-      title: "<strong><u>¡ATENCION!</u></strong>",
-      icon: "info",
+      title: '<strong><u>¡ATENCION!</u></strong>',
+      icon: 'info',
       html: `
         Para agregar un trago a tu Bodega Personal necesitas estar logueado
       `,
@@ -133,19 +102,18 @@ export class CoctelListComponent implements OnInit {
 
   positiveAddCocktailAlert(): void {
     Swal.fire({
-      icon: "success",
-      title: "Trago agregado a tu Bodega Personal",
+      icon: 'success',
+      title: 'Trago agregado a tu Bodega Personal',
       showConfirmButton: false,
-      timer: 2000
+      timer: 2000,
     });
   }
 
   negativeAddCocktailAlert(): void {
     Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "El trago ya pertenece a tu Bodega Personal"
+      icon: 'error',
+      title: 'Oops...',
+      text: 'El trago ya pertenece a tu Bodega Personal',
     });
   }
-
 }
--- src/app/services/bodega-personal-api.service.ts
+++ src/app/services/bodega-personal-api.service.ts
@@ -2,43 +2,43 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, lastValueFrom, map } from 'rxjs';
 import { BodegaPersonal, User } from './models';
+import { IBodegaPersonal } from './interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BodegaPersonalService {
-
-  public baseURL: string = "http://localhost:3000/bodega_personal";
-
+  public baseURL: string = 'http://localhost:3000/bodega-personal';
 
   private bodegaPersonal: BodegaPersonal | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public addCocktailToBodegaPersonal(bodegaPersonal: BodegaPersonal): Observable<BodegaPersonal> {
-    return this.http.post<BodegaPersonal>(`${this.baseURL}`, bodegaPersonal)
-
+  public addCocktailToBodegaPersonal(
+    bodegaPersonal: IBodegaPersonal
+  ): Observable<IBodegaPersonal> {
+    return this.http.post<IBodegaPersonal>(`${this.baseURL}`, bodegaPersonal);
   }
 
-  public tragoYaExisteEnBodegaPersonal(bodegaPersonal : BodegaPersonal): Observable<boolean>{
+  public tragoYaExisteEnBodegaPersonal(
+    bodegaPersonal: IBodegaPersonal
+  ): Observable<boolean> {
     return this.getCocktails().pipe(
-      map(cocktails => cocktails.some(item => item.drinkId === bodegaPersonal.drinkId))
+      map((cocktails) =>
+        cocktails.some((item) => item.drinkId === bodegaPersonal.drinkId)
+      )
     );
   }
 
   //Get all de todos los cocktails de la bodega personal de un usuario
   public getCocktails(): Observable<BodegaPersonal[]> {
     return this.http.get<BodegaPersonal[]>(`${this.baseURL}`);
-  } 
-
-  
+  }
 
   //me traigo todos los objetos de bodega personal y los filtro por el userId del usuario logueado, falta hacer la logica
   public getCocktailsFromUser(userId: string): Observable<BodegaPersonal[]> {
     return this.getCocktails().pipe(
-      map(cocktails => cocktails.filter(item => item.userId === userId))
+      map((cocktails) => cocktails.filter((item) => item.userId === userId))
     );
   }
-
-
 }
